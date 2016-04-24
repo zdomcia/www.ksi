67d2f0ba3b47cb30f37d68c08f574743e9fc4279
Daniel Skowroński <daniel@dsinf.net>
2016-04-24 17:15:14 +0200

    #1 again - custom menu that time :D

--- html/assets/js/main.js
+++ html/assets/js/main.js
@@ -12,6 +12,7 @@
 		xlarge:	'(max-width: 1680px)',
 		large:	'(max-width: 1280px)',
 		medium:	'(max-width: 980px)',
+		menucritical:	'(max-width: 850px)',
 		small:	'(max-width: 736px)',
 		xsmall:	'(max-width: 480px)'
 	});
@@ -62,104 +63,29 @@
 			}
 
 		// Menu.
-			var $menu = $('#menu');
+			var $menu = $('#header ul.links');
+			var $menuButton = $('#header nav a#menu-button');
+			var menuIsOpen = false;
 
-			$menu._locked = false;
+			if (skel.breakpoints('menucritical').active) $menu.hide();
+			
+			$( $menuButton ).click(function() {
+				menuIsOpen = !menuIsOpen;
 
-			$menu._lock = function() {
+				if (menuIsOpen){
+					$menu.show();
+				}
+				else{
+					$menu.hide();
+				}
+			});
 
-				if ($menu._locked)
-					return false;
-
-				$menu._locked = true;
-
-				window.setTimeout(function() {
-					$menu._locked = false;
-				}, 350);
-
-				return true;
-
-			};
-
-			$menu._show = function() {
-
-				if ($menu._lock())
-					$body.addClass('is-menu-visible');
-
-			};
-
-			$menu._hide = function() {
-
-				if ($menu._lock())
-					$body.removeClass('is-menu-visible');
-
-			};
-
-			$menu._toggle = function() {
-
-				if ($menu._lock())
-					$body.toggleClass('is-menu-visible');
-
-			};
-
-			$menu
-				.appendTo($body)
-				.on('click', function(event) {
-
-					event.stopPropagation();
-
-					// Hide.
-						$menu._hide();
-
-				})
-				.find('.inner')
-					.on('click', '.close', function(event) {
-
-						event.preventDefault();
-						event.stopPropagation();
-						event.stopImmediatePropagation();
-
-						// Hide.
-							$menu._hide();
-
-					})
-					.on('click', function(event) {
-						event.stopPropagation();
-					})
-					.on('click', 'a', function(event) {
-
-						var href = $(this).attr('href');
-
-						event.preventDefault();
-						event.stopPropagation();
-
-						// Hide.
-							$menu._hide();
-
-						// Redirect.
-							window.setTimeout(function() {
-								window.location.href = href;
-							}, 350);
-
-					});
-
-			$body
-				.on('click', 'a[href="#menu"]', function(event) {
-
-					event.stopPropagation();
-					event.preventDefault();
-
-					// Toggle.
-						$menu._toggle();
-
-				})
-				.on('keydown', function(event) {
-
-					// Hide on escape.
-						if (event.keyCode == 27)
-							$menu._hide();
-
-				});
+			skel.on('-menucritical', function() {
+				$menu.show();
+			});
+			skel.on('+menucritical', function() {
+				$menu.hide();
+			});
 
 	});
 
