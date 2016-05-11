c13e2f05023c88d2e5af88abaaaeacfd9f2cba13
Daniel Skowroński <daniel@dsinf.net>
2016-05-11 17:51:47 +0200

    imported jekyll_i18n plugin and started implementing it

--- jekyll/js/main.js
+++ jekyll/js/main.js
@@ -21,7 +21,7 @@ function hidePhoto(){
 		xlarge:	'(max-width: 1680px)',
 		large:	'(max-width: 1280px)',
 		medium:	'(max-width: 980px)',
-		menucritical:	'(max-width: 900px)',
+		menucritical:	'(max-width: 950px)',
 		small:	'(max-width: 736px)',
 		xsmall:	'(max-width: 480px)'
 	});
